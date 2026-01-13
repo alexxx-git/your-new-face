d075c08a071b03527cf84923889679030757c7d0
alexxx-git <alexxx.tot@gmail.com>
2026-01-13 03:47:20 +0300

    add create_bot starthandler and start bot

---
+++ new_face/telebot/create_bot.py
@@ -0,0 +1,21 @@
+import logging
+import os
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+dotenv_path = Path(__file__).parent.parent.parent / ".env"
+load_dotenv(dotenv_path)
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+bot = Bot(token=BOT_TOKEN,default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+dp = Dispatcher(storage=MemoryStorage())
